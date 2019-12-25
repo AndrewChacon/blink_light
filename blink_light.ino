643846d1de8f146fde4bed399ae90d40798ec15d
AndrewChacon <andrew.b.chacon@gmail.com>
2019-12-25 17:09:18 -0500

    blink 2 lights using for loop

--- blink_light.ino
+++ blink_light.ino
@@ -1,24 +1,30 @@
-int redLED=9;
-int blueLED=10;
-//int onTime=500;
-//int offTime=500;
+int blueLEDPin=9;
+int redLEDPin=10;
+int blueOnTime=250;
+int blueOffTime=250;
+int redOnTime=250;
+int redOffTime=250;
 
 void setup() {
   // put your setup code here, to run once:
-  
-  pinMode(redLED, OUTPUT);
-  pinMode(blueLED, OUTPUT);
+
+  pinMode(blueLEDPin, OUTPUT);
+  pinMode(redLEDPin, OUTPUT);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-//  digitalWrite(blueLED, HIGH);
-//  delay(onTime);
-//  digitalWrite(blueLED, LOW);
-//  delay(offTime);
 
-  digitalWrite(redLED, HIGH);
-  digitalWrite(blueLED, HIGH);
+  for(int j=0; j<=1; j=j+1){
+    digitalWrite(blueLEDPin, HIGH);
+    delay(blueOnTime);
+    digitalWrite(blueLEDPin, LOW);
+    delay(blueOffTime);
+  }
+
+  digitalWrite(redLEDPin, HIGH);
+  delay(redOnTime);
+  digitalWrite(redLEDPin, LOW);
+  delay(redOffTime);
 }
